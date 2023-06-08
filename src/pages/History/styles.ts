4dd3b09feb82863e6fd56fdb3eb1ec217e2fdac1
Nadi Duno <nadiduno@gmail.com>
2023-06-08 20:41:17 -0300

    History

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const HistoryContainer = styled.main`
+    flex: 1;
+    padding: 3.5rem;
+    display: flex;
+    flex-direction: column;
+`
+
+export const HistoryList = styled.div`
+    flex:1;
+    overflow: auto;
+    margin-top: 2rem;
+    table{
+        width: 100%;
+        border-collapse: collapse;
+        min-width: 600px;
+        th{
+            background-color: ${(props) => props.theme['gray-600']};
+            color: ${(props) => props.theme['gray-100']};
+            padding: 1rem;
+            text-align: left;
+            font-size: 0.875rem;
+            line-height: 1.6;
+        &:first-child{
+            border-top-left-radius: 8px;
+            padding-left: 1.5rem;
+        }
+        &:last-child{
+            border-top-right-radius: 8px;
+            padding-right: 1.5rem;
+        }
+        }
+    td{
+        background-color: ${(props) => props.theme['gray-200']};
+        color: ${(props) => props.theme['gray-500']};
+        border-top: 4px solid ${(props) => props.theme['pastel-100']};
+        padding: 1rem;
+        font-size: 0.875rem;
+        line-height: 1.6;
+        &:first-child{
+            width: 50%;
+            padding-left: 1.5rem;
+        }
+        &:last-child{
+            padding-right: 1.5rem;
+        }
+    }
+    }
+`
